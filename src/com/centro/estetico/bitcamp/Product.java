6d3e21040cee3c27c5491a4742b8a087fb8c6a85
Marta992 <166708514+Marta992@users.noreply.github.com>
2024-09-30 19:05:19 +0200

    classe prodotto, enum prodotto

---
+++ src/com/centro/estetico/bitcamp/Product.java
@@ -0,0 +1,98 @@
+package com.centro.estetico.bitcamp;
+
+import java.math.BigDecimal;
+
+public class Product {
+	int id;
+	String name;
+	int amount;
+	int minStock;
+	BigDecimal price;
+	double vat;
+	ProductCat type;
+	boolean isEnabled;
+	
+	
+	public Product(int id, String name, int amount, int minStock, BigDecimal price, double vat, ProductCat type,
+			boolean isEnabled) {
+		
+		this.id = id;
+		this.name = name;
+		this.amount = amount;
+		this.minStock = minStock;
+		this.price = price;
+		this.vat = vat;
+		this.type = type;
+		this.isEnabled = isEnabled;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	public int getMinStock() {
+		return minStock;
+	}
+	
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+	
+	public BigDecimal getPrice() {
+		return price;
+	}
+	
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+	public double getVat() {
+		return vat;
+	}
+	
+	public void setVat(double vat) {
+		this.vat = vat;
+	}
+	
+	public ProductCat getType() {
+		return type;
+	}
+	
+	public void setType(ProductCat type) {
+		this.type = type;
+	}
+	
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+	
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", amount=" + amount + ", minStock=" + minStock + ", price="
+				+ price + ", vat=" + vat + ", type=" + type + ", isEnabled=" + isEnabled + "]";
+	}
+	
+}
---
+++ src/com/centro/estetico/bitcamp/ProductCat.java
@@ -0,0 +1,21 @@
+package com.centro.estetico.bitcamp;
+
+public enum ProductCat {
+ORAL_CARE("Cura Orale"),
+SKIN_CARE("Cura Pelle"),
+HAIR_CARE("Cura Capelli"),
+BODY_CARE("Cura del corpo"),
+COSMETICS("Cosmetici"),
+PERFUMES("Profumi"),
+OHER("Altro");
+	
+	private final String description;
+
+	private ProductCat(String description ) {
+		this.description = description;
+	}
+	public String getDescription() {
+		return description;
+	}
+}
+
